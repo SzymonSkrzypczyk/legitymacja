72ba3d0954738f858bd85f68266f2c5305b967fc
szymon <Szymon.Skrzypczyk@vp.pl>
2022-11-29 18:09:59 +0100

    dziala jak powinno

--- back_page.py
+++ back_page.py
@@ -1,4 +1,4 @@
-from typing import Union, Iterable
+from typing import Union, Set, List
 from pathlib import Path
 from PIL import Image
 
@@ -9,6 +9,7 @@ Do zrobienia:
 2.2) obliczanie miejsc na naklejki V
 2.3) zmiana rozmiaru naklejek V
 3) przetwarzanie wielu naklejek X
+4) naprawienie ilosci rzedow i kolumn X
 """
 
 SAMPLE = Path(__file__).parent / 'images' / 'tyl_legitka.jpeg'
@@ -35,24 +36,38 @@ def place_sticker(im: Image, sticker: Union[str, Path], row: int = 0, col: int =
     return im
 
 
-def process_stickers(image: Union[str, Path], stickers: Iterable[Union[str, Path]]):
+def process_stickers(image: Union[str, Path],
+                     stickers: Union[Set[Union[str, Path]], List[Union[str, Path]]],
+                     ):
     """
     Zla ilosc kolumn i rzedow!
     """
     image = Path(image)
     image = Image.open(image)
+    '''prawie dziala
     col = 0
     row = 0
     for ind, i in enumerate(stickers, 0):
         sticker = Path(i)
         image = place_sticker(image, sticker, row, col)
+        print(row, col)
         if ind % 2 == 0:
             col += 1
         row += 1
-        row %= 2
+        row %= 4
+        col %= 3'''
+    row = 0
+    col = 0
+    for i in stickers:
+        if col == 3:
+            col = 0
+            row += 1
+        sticker = Path(i)
+        image = place_sticker(image, sticker, row, col)
+        col += 1
     image.show()
 
 
 if __name__ == '__main__':
     # place_sticker(SAMPLE, SAMPLE_STICKER, 1, 2)
-    process_stickers(SAMPLE, 9 * [SAMPLE_STICKER])
+    process_stickers(SAMPLE, 5 * [SAMPLE_STICKER])
