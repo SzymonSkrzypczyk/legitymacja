2ede6d778ef26906fcc5f87ebcd16e6eb4efc3c5
szymon <Szymon.Skrzypczyk@vp.pl>
2022-11-30 10:27:27 +0100

    kolejne puste testy

---
+++ tests/test_back_page.py
@@ -0,0 +1,14 @@
+import pytest
+from back_page import _resize_sticker, _place_sticker, back_page
+
+
+def test_return_resize_sticker():
+    ...
+
+
+def test_return_place_sticker():
+    ...
+
+
+def test_return_back_page():
+    ...
---
+++ tests/test_full_page.py
@@ -0,0 +1,14 @@
+import pytest
+from create_full import main, _merge_photos, _resize
+
+
+def test_return_merge():
+    ...
+
+
+def test_return_resize():
+    ...
+
+
+def test_return_main():
+    ...
