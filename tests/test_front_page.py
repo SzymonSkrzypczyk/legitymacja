17e3159ff5262976ab7b00c8cb0cc403587d614b
szymon <Szymon.Skrzypczyk@vp.pl>
2022-11-30 10:21:37 +0100

    poczatek testowania

---
+++ tests/test_front_page.py
@@ -0,0 +1,26 @@
+import pytest
+from front_page import _get_resized_face, _paste_in_image, front_page, _add_university, _add_chip, _add_text
+
+
+def test_return_front():
+    ...
+
+
+def test_return_resized():
+    ...
+
+
+def test_return_paste_in():
+    ...
+
+
+def test_return_university():
+    ...
+
+
+def test_return_chip():
+    ...
+
+
+def test_return_text():
+    ...
